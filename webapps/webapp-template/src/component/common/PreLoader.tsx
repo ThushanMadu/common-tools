fc77285484a5c8f83d2067f7cb8dfa96543187d5
dinethde <dinethran33@gmail.com>
2025-11-17 20:06:43 +0530

    fix: update CustomCircularProgress to use indeterminate variant

--- webapps/webapp-template/src/component/common/PreLoader.tsx
+++ webapps/webapp-template/src/component/common/PreLoader.tsx
@@ -30,16 +30,6 @@ function CustomCircularProgress(props: CircularProgressProps) {
   return (
     <Box sx={{ position: "relative" }}>
       <CircularProgress
-        variant="determinate"
-        sx={{
-          color: (theme) => theme.palette.grey[800],
-        }}
-        size={40}
-        thickness={4}
-        {...props}
-        value={100}
-      />
-      <CircularProgress
         variant="indeterminate"
         disableShrink
         sx={{
